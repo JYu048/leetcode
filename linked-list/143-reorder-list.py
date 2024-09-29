24947fe919bff3989a5d708945dcbff696bddd3b
Jerry Yu <JaredY3325@Gmail.com>
2024-09-28 22:49:59 -0600

    141. Linked List Cycle

--- linked-list/143-reorder-list.py
+++ linked-list/143-reorder-list.py
@@ -47,3 +47,53 @@ class Solution:
 
 # Time: O(N)
 # Space: O(1)
+
+
+
+# Optimal Solution (from chatgpt)
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        
+        # find middle
+        slow, fast = head, head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        
+
+        # reverse right half
+        prev, curr = None, slow
+
+        while curr:
+            next_node = curr.next
+            curr.next = prev
+            prev, curr = curr, next_node
+        
+        # Merge two lists
+        left, right = head, prev
+
+        while right.next:
+            next_left, next_right = left.next, right.next
+
+            left.next = right
+            right.next = next_left
+
+            left, right = next_left, next_right
+        
+        return head
+
+        
+        
+        
+
+        
